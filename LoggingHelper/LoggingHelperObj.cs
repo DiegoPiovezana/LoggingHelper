0579353c9749dd85fe60f1aaee5e2b56a8090d5f
diegopiovezana <diegopiovezana@gmail.com>
2024-02-18 10:54:15 -0300

    feat: possibility of using library in a non-static way through LoggingHelperObj

--- LoggingHelper/LoggingHelperObj.cs
+++ LoggingHelper/LoggingHelperObj.cs
@@ -1,76 +1,59 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LH
 {
     /// <summary>
     /// Library for logging.
     /// </summary>
-    public static class LoggingHelper
+    public class LoggingHelperObj
     {
-        /// <summary>
-        /// The default log object for static use.
-        /// </summary>
-        public static readonly LoggingHelperObj LogDefault = new LoggingHelperObj();
-
         /// <summary>
         /// Maximum hierarchical level to be logged in the method stack (final index). Example: 5
         /// </summary>
-        public static int LevelStack
-        {
-            get => LogDefault.LevelStack;
-            set { LogDefault.LevelStack = value; }
-        }
+        public int LevelStack { get; set; } = 4;
 
         /// <summary>
         /// The number of days after which the log file should be considered outdated and will be deleted. Default is 3 days.
         /// </summary>
-        public static int LogValidity
-        {
-            get => LogDefault.LogValidity;
-            set { LogDefault.LogValidity = value; }
-        }
+        public int LogValidity { get; set; } = 3;
 
         /// <summary>
         /// Minimum level to be logged in the log file.
         /// <para>Example: If set to 2, TRACE and DEBUG logs will not be logged.</para>
         /// <para>Enter a negative number to avoid recording logs.</para>
         /// </summary>
-        public static int LogLevelFile
-        {
-            get => LogDefault.LogLevelFile;
-            set { LogDefault.LogLevelFile = value; }
-        }
+        public int LogLevelFile { get; set; } = 0;
 
         /// <summary>
         /// Minimum level to be logged in the log console.
         /// <para>Example: If set to 2, TRACE and DEBUG logs will not be logged.</para>
         /// <para>Enter a negative number to avoid recording logs.</para>
         /// </summary>
-        public static int LogLevelConsole
-        {
-            get => LogDefault.LogLevelConsole;
-            set { LogDefault.LogLevelConsole = value; }
-        }
+        public int LogLevelConsole { get; set; } = -1;
+
+
+        private bool _firstChecked = false;
+        private string _logPath = ".\\LOG_LH\\Logging_general.log";
 
         /// <summary>
         /// Location where the log file will be stored.
         /// (Specify a different location or filename to use different log files).
         /// </summary>
-        public static string LogPathFile
+        public string LogPathFile
         {
-            get => LogDefault.LogPathFile;
-            set { LogDefault.LogPathFile = value; CheckFile(LogValidity); }
+            get => _logPath;
+            set { _logPath = value; CheckFile(LogValidity); }
         }
 
         /// <summary>
         /// The format of the log message.
-        /// </summary>        
-        public static string FormatLogOutput
-        {
-            get => LogDefault.FormatLogOutput;
-            set { LogDefault.FormatLogOutput = value; }
-        }
+        /// </summary>
+        public string FormatLogOutput { get; set; } = "<dd/MM/yyyy HH:mm:ss.fff> [<level>] (<stack>) <message> | <obs>"; // "{0} [{1}] ({2}) {3}{4}"
+
 
         /// <summary>
         /// Log levels.
@@ -108,15 +91,6 @@ namespace LH
             CRITICAL
         }
 
-        /// <summary>
-        /// Create a new log object.
-        /// </summary>
-        /// <returns></returns>
-        public static LoggingHelperObj NewLog()
-        {
-            return new LoggingHelperObj();
-        }
-
 
 
 
@@ -126,19 +100,44 @@ namespace LH
         /// </summary>
         /// <param name="days">The number of days after which the log file should be considered outdated. Default is 3 days.</param>
         /// <param name="hidden">Specifies whether the directory should be hidden. Default is true.</param>
-        public static bool CheckFile(int days = 3, bool hidden = true)
+        public bool CheckFile(int days = 3, bool hidden = true)
         {
-            return LogDefault.CheckFile(days, hidden);
+            _firstChecked = true;
+
+            DirectoryInfo di = Directory.CreateDirectory(Path.GetDirectoryName(LogPathFile));
+            if (hidden) di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            else di.Attributes = FileAttributes.Directory & ~FileAttributes.Hidden;
+
+            if (File.Exists(LogPathFile))
+            {
+                DateTime creationLog = File.GetCreationTime(LogPathFile);
+                if ((DateTime.Now - creationLog).TotalDays > days) DeleteLogFile();
+            }
+            return true;
         }
 
         /// <summary>
         /// Deletes the log file defined in the LogPath attribute.
         /// </summary>
         /// <returns>True if the file was successfully deleted, otherwise false.</returns>
-        public static bool DeleteLogFile()
+        public bool DeleteLogFile()
         {
-            return LogDefault.DeleteLogFile();
-        }
+            if (File.Exists(LogPathFile))
+            {
+                try
+                {
+                    File.Delete(LogPathFile);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    // Handle exception or log error if necessary
+                    return false;
+                }
+            }
+
+            return true; // If the file doesn't exist, consider it as deleted
+        }        
 
         /// <summary>
         /// Write a message to the log file.
@@ -147,12 +146,30 @@ namespace LH
         /// <param name="level">Set the level of this log. TRACE = 0, DEBUG = 1, INFO = 2, WARNING = 3, ERROR = 4, CRITICAL = 5</param>
         /// <param name="obs">Provide any additional information you deem necessary (optional)</param>
         /// <returns>Returns true if the log was written successfully; otherwise, returns false</returns>
-        public static bool Write(string message, object level, string obs)
+        public bool Write(string message, object level, string obs)
         {
-            return LogDefault.Write(message, level, obs);
+            int intLevelMessage = Treatment.GetIntLevelMessage(level);
+
+            string callingMethod = GetCallingMethodName(2, LevelStack);
+
+            if (LogLevelConsole >= 0 && intLevelMessage >= LogLevelConsole)
+            {
+                WriteLog.ToConsole(message, ((Level)intLevelMessage).ToString(), callingMethod, obs);
+            }
+
+            if (LogLevelFile >= 0 && intLevelMessage >= LogLevelFile)
+            {
+                if (!_firstChecked) { CheckFile(LogValidity); } // TODO: change to when to boot                
+
+                Task<bool> task = Task.Run(() => WriteLog.ToFile(LogPathFile, message, ((Level)intLevelMessage).ToString(), callingMethod, obs));
+
+                // Wait for log writing for a maximum of 5 seconds
+                if (task.Wait(TimeSpan.FromSeconds(5))) return task.Result;
+                else return false; // If the time has expired, do not write
+            }
+
+            return true;
         }
-
-
 
         /// <summary>
         /// Identifies the name of the calling method (stack).
@@ -160,10 +177,21 @@ namespace LH
         /// <param name="indStack">Initial index for the call stack. E.g., if 2, it analyzes from the antepenultimate call.</param>
         /// <param name="levelPath">Maximum hierarchical level to be recorded in the method path (end index). E.g., 5.</param>
         /// <returns>The name of the calling method (stack).</returns>
-        public static string GetCallingMethodName(int indStack, int levelPath)
+        public string GetCallingMethodName(int indStack, int levelPath)
         {
-            return LogDefault.GetCallingMethodName(indStack, levelPath);
+            StackFrame frame = new StackFrame(indStack);
+            StringBuilder nameMethod = new StringBuilder();
+
+            for (int i = 1; frame.GetILOffset() != -1 && i <= levelPath; i++)
+            {
+                nameMethod.Insert(0, "/" + frame.GetMethod().Name); // Get the name of the calling method
+
+                frame = new StackFrame(indStack + i); // Next previous frame
+            }
+
+            return nameMethod.ToString();
         }
+
 
 
     }
